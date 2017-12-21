d93482ea44c2dc431b179478ba61979fa73915dd
Thomas A. Pepperz <thomas.peppers@colorado.edu>
2017-12-20 20:21:51 -0600

    Computes percentage of IOTA mktCap
    Adds a vanity function which takes in the amount of user-owned IOTA and outputs the percentage of the total IOTA supply the user owns.

---
+++ perOfIotaSply.R
@@ -0,0 +1,6 @@
+perOfIotaSply <-
+function(quant, iotUnit){
+    divisor <- iotUnit
+    answer <- ((quant * iotUnit)/iotaSply)*100
+    paste('You own ', answer, '% of the total iota supply.', sep="")
+  }
